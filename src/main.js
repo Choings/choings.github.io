b21e53ce263d1844ea81790dd08ee7201b8e01cd
Choings <seokeee92@naver.com>
2024-02-12 11:05:38 +0900

    JS-Home

--- src/main.js
+++ src/main.js
@@ -10,6 +10,13 @@ document.addEventListener('scroll', () => {
     } else {
         header.classList.remove('header--dark');
     }
-})
+});
+
+// Home 섹션을 아래로 스크롤시 투명하게 처리함
+const home = document.querySelector('.home__container');
+const homeHeight = home.offsetHeight;
+document.addEventListener('scroll', () => {
+    home.style.opacity = 1 - window.scrollY / homeHeight;
+});
 
 
